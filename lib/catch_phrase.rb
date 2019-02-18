69ebeb72d3758ee7640fd5686e7a5cce487a5dcb
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-18 01:58:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,11 @@
 def catch_phrase
   puts phrase
 end
+
+evil_monster = "Bowser"
+ 
+def princess_peaches_castle(evil_monster)
+  puts "#{evil_monster} is trying to kidnap Princess Peach!"
+end
+ 
+princess_peaches_castle(evil_monster)
